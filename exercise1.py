cd4590392dfc710d08d5ad1a1a30bb8aba71db4d
littlehippos <littlehippos@gmail.com>
2016-09-15 13:09:19 -0700

    Added hi_fellow function

--- exercise1.py
+++ exercise1.py
@@ -1,2 +1,5 @@
 def print_message():
-    print 'help me!'
+    print "help me!"
+
+def hi_fellow(input = "I am an Insight Fellow!"):
+    return "That's awesome How is that random forest model going?"
